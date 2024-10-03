c6cfd3897bc62e79b9d827a8379e2c967f39bfa5
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2024-10-03 18:33:23 +0300

    completed homework number 3 and 4

--- index.ts
+++ index.ts
@@ -3,8 +3,6 @@
 // Вихід: [3, 5, 6, 7]
 // Пояснення: Числа в діапазоні [1, 7], які діляться на 3, 5 або 7, це 3, 5, 6, 7. Сума цих чисел дорівнює 21.
 
-const n = 7;
-
 const getArrayMultiplesNumbers = (numberCount: number) => {
   let arr: number[] = [];
 
@@ -19,6 +17,7 @@ const getArrayMultiplesNumbers = (numberCount: number) => {
   return arr;
 };
 
+const n = 7;
 console.log(getArrayMultiplesNumbers(n));
 
 // Завдання №2
@@ -30,12 +29,49 @@ console.log(getArrayMultiplesNumbers(n));
 // Вихід: false
 // Пояснення: Зліва направо читається як -121. Справа наліво стає 121-, тому це не паліндром.
 
-const x = 121;
-
 const isPalindrome = (number: number) => {
   const str = number.toString();
   const strReverse = str.split('').reverse().join('');
   return str === strReverse;
 };
 
+const x = 121;
 console.log(isPalindrome(x));
+
+// Завдання №3
+// Напишіть функцію delay(ms), яка повертає проміс, що виконується через ms мілісекунд.
+
+const delay = (ms: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+// Виклик функції
+delay(2000).then(() => console.log('Пройшло 2 секунди'));
+
+// Завдання №4
+// Ланцюг математичних операцій з Promises.
+// Дано число 5.
+// Кожна операція повинна бути в окремому промісі, і ці проміси слід з'єднати ланцюгом. Спочатку подвоїти його, потім додати 10.
+
+// проміси тут
+const double = (number: number) => {
+  return new Promise((resolve) => {
+    resolve(number * 2);
+  });
+};
+
+const addTen = (number: number) => {
+  return new Promise((resolve) => {
+    resolve(number + 10);
+  });
+};
+
+const value = 5;
+
+double(value)
+  .then(addTen)
+  .then((result) => {
+    console.log(result); // 20
+  });
